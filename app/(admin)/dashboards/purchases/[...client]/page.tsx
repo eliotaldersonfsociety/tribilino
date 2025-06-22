84d410d62a430c455f7ff100188df311eb6a0b65
eliotaldersonfsociety <aldersonelliot803@gmail.com>
2025-06-22 09:39:33 -0500

    volvi aja vamos aja

--- app/(admin)/dashboards/purchases/[...client]/page.tsx
+++ app/(admin)/dashboards/purchases/[...client]/page.tsx
@@ -1,18 +1,10 @@
 import { getAllPurchases } from "@/app/helpers/getAllPurchases";
 import PurchasesClientPage from "./PurchasesClientPage";
 
-interface Props {
-  searchParams?: Record<string, string | string[] | undefined>;
-}
+export default async function PurchasesPage({ searchParams }: any) {
+  const page = Number(searchParams?.page) || 1;
 
-export default async function PurchasesPage({ searchParams }: Props) {
-  const paginaParam = Array.isArray(searchParams?.pagina)
-    ? searchParams?.pagina[0]
-    : searchParams?.pagina;
+  const { purchases, pagination } = await getAllPurchases({ page });
 
-  const page = parseInt(paginaParam || "1", 10);
-
-  const initialPurchases = await getAllPurchases({ page });
-
-  return <PurchasesClientPage initialPurchases={initialPurchases} />;
+  return <PurchasesClientPage initialPurchases={{ purchases, pagination }} />;
 }
