de63172787d07baba77aa202be45e310260a0e06
eliotaldersonfsociety <aldersonelliot803@gmail.com>
2025-06-22 17:29:00 -0500

    Update PanelPageClient.tsx

--- app/(admin)/dashboards/PanelPageClient.tsx
+++ app/(admin)/dashboards/PanelPageClient.tsx
@@ -32,7 +32,8 @@ const PanelPageClient: React.FC<PanelPageClientProps> = ({
           Error: productos inválidos en tu carrito.
         </div>
       ) : (
-      <div className="w-full max-w-[1200px] mx-auto flex flex-col gap-4 p-4 md:p-8 bg-gray-100">
+      <div className="w-full bg-gray-100 min-h-screen">
+      <div className="w-full max-w-[1200px] mx-auto flex flex-col gap-4 p-4 md:p-8">
         <div className="flex flex-col gap-4 p-4 md:p-8">
           {/* Cards principales */}
           <div className="grid gap-4 grid-cols-1 sm:grid-cols-2 lg:grid-cols-4">
@@ -98,6 +99,7 @@ const PanelPageClient: React.FC<PanelPageClientProps> = ({
             <></>
           </DashboardLayouts>
 
+        </div>
         </div>
         </div>
       )}
