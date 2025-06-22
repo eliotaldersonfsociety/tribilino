be44bcd3d7fe15817b322024b56f9f0f29cc1810
eliotaldersonfsociety <aldersonelliot803@gmail.com>
2025-06-22 17:33:37 -0500

    Update page.tsx

--- app/(admin)/dashboards/estadisticas/page.tsx
+++ app/(admin)/dashboards/estadisticas/page.tsx
@@ -7,6 +7,8 @@ export default async function AnalyticsPage() {
 
   return (
     <>
+      <div className="w-full bg-gray-100 min-h-screen">
+      <div className="w-full max-w-[1200px] mx-auto flex flex-col gap-4 p-4 md:p-8">
       <main className="container mx-auto py-8 px-4">
         <h1 className="text-3xl font-bold mb-8">Panel de Análisis Web</h1>
         <WebAnalyticsClient stats={stats} />
@@ -14,6 +16,8 @@ export default async function AnalyticsPage() {
     <DashboardLayouts>
       <></>
     </DashboardLayouts>
+        </div>
+        </div>
     </>
   );
 }
